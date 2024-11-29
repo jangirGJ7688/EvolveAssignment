1b55abfcd2d0f41f6bbb347e7a79ce0a4655bf5a
Ganpat Jangir <ganpatlaljangid7688@gmail.com>
2024-11-29 22:32:54 +0530

    Initial Commit

---
+++ EvolveAssignment/EvolveAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EvolveAssignmentApp.swift
+//  EvolveAssignment
+//
+//  Created by Ganpat Jangir on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EvolveAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
